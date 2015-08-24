c2abcffcaea5807555e3a7deaa2c3ad7ed62b697
AM1300 <lamprinivasilaki@gmail.com>
2015-08-24 15:36:21 +0300

    format complex numbers from database

--- api/controllers/transformer.js
+++ api/controllers/transformer.js
@@ -1,4 +1,5 @@
 var moment = require('moment');
+var math = require('mathjs');
 
 module.exports = {
   getTransformerRecords: function(req, res, next) {
@@ -8,25 +9,31 @@ module.exports = {
     var hours =  parseInt(time.split(':')[0]);
     var mins  =  parseInt(time.split(':')[1]);
 
-
     var parsedDate = moment(date, "DD-MM-YYYY");
     parsedDate.add(hours, 'hours');
     parsedDate.add(mins, 'minutes');
     var formattedDate = parsedDate.format("YYYY-MM-DD HH:mm:ss");
-    // var formattedDateNext = parsedDate.add(1, 'days').format("YYYY-MM-DD HH:mm:ss");
 
-    // mySqlConnection.query('SELECT * FROM transformer WHERE timestamp >= "'+formattedDate+'" && timestamp < "'+formattedDateNext+'" ',
     mySqlConnection.query('SELECT * FROM transformer WHERE timestamp = "'+formattedDate+'" ',
-  function(err, rows, fields) {
-      if (err){
-        throw err;
-      }
-      else {
-        var modifiedRows = rows.map(function(row){
-          return row;
-        });
-        res.json(modifiedRows);
-      }
-    });
+      function(err, rows, fields) {
+          if (err){
+            throw err;
+          }
+          else {
+            var modifiedRows = rows.map(function(row){
+              // sails.log(rows[0].power_in_A_real);
+              // row.phaseAIn = math.complex(1, 2).toString();
+              row.phaseAIn = math.complex(rows[0].power_in_A_real, rows[0].power_in_A_imag).toString();
+              row.phaseAOut = math.complex(rows[0].power_out_A_real, rows[0].power_out_A_imag).toString();
+              row.phaseALosses = math.complex(rows[0].power_losses_A_real, rows[0].power_losses_A_imag).toString();
+              row.phaseBIn = math.complex(rows[0].power_in_B_real, rows[0].power_in_B_imag).toString();
+              row.phaseBOut = math.complex(rows[0].power_out_B_real, rows[0].power_out_B_imag).toString();
+              row.phaseCIn = math.complex(rows[0].power_in_C_real, rows[0].power_in_C_imag).toString();
+              row.phaseCOut = math.complex(rows[0].power_out_C_real, rows[0].power_out_C_imag).toString();
+              return row;
+            });
+            res.json(modifiedRows);
+          }
+      });
   }
 };
